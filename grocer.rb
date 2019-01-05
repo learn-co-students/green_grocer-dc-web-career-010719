a7e884b5aa5d3fd378886401c37108a246124227
smaraghi <maraghiserven@gmail.com>
2019-01-05 18:00:58 -0500

    add main pages text content, image, and links

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,47 @@
 def consolidate_cart(cart)
-  # code here
+  new_hash = {}
+  cart.each do |c|
+    c.each do |food, info|
+      if new_hash.keys.include?(food)
+        new_hash[food][:count] += 1
+      else
+        new_hash[food] = {
+          :price => info[:price],
+          :clearance => info[:clearance],
+          :count => 1
+        }
+      end
+    end
+  end
+
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |c|
+    if cart.keys.include?(c[:item])
+      counter = 0
+      while c[:num] < cart[c[:item]][:count]
+        cart[c[:item]][:count] -= c[:num]
+        cart["#{c[:item].upcase} W/COUPON"] = {
+          :price => c[:cost],
+          :clearance => cart[c[:item]][:clearance],
+          :count => count += 1
+        }
+      end
+    end
+  end
+
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |k, v|
+    if cart[k][:clearance] == true
+      new_cart = cart[k][:price] = cart[k][:price] * 0.8
+      new_cart.round
+    end
+  end
 end
 
 def checkout(cart, coupons)
